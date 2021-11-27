7fa144d797722ef74916b2bfc135fa0a079b186f
mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2021-11-27 14:51:00 +0200

    split down every component of the homepage into its own component files

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Direcotry from '../../components/directory/directory.component'
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+  <div className="homepage">
+    <Direcotry />
+  </div>
+)
+
+export default HomePage;
