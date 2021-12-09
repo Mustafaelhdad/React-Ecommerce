73712298f5fc2ca039c870cdbcb50a359c97d5ad
mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2021-12-09 14:10:19 +0200

    added sign up using email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyAHh5bJa20d4g_NwreRUf6xkZtxvYiBHvg",
@@ -9,40 +9,40 @@ const config = {
   storageBucket: "cr0wn-db.appspot.com",
   messagingSenderId: "179770859158",
   appId: "1:179770859158:web:1ced4db17a52443eceecb7",
-  measurementId: "G-MMH30K6456"
-}
+  measurementId: "G-MMH30K6456",
+};
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`)
-  const snapShop = await userRef.get()
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShop = await userRef.get();
 
   if (!snapShop.exists) {
-    const { displayName, email } = userAuth
-    const createdAt = new Date()
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
     try {
       await userRef.set({
         displayName,
         email,
         createdAt,
-        ...additionalData
-      })
+        ...additionalData,
+      });
     } catch (error) {
-      console.log('error creating user', error.message)
+      console.log("error creating user", error.message);
     }
   }
 
-  return userRef
-}
+  return userRef;
+};
 
-firebase.initializeApp(config)
+firebase.initializeApp(config);
 
-export const auth = getAuth()
-export const firestore = firebase.firestore()
+export const auth = getAuth();
+export const firestore = firebase.firestore();
 
-const provider = new GoogleAuthProvider()
-export const signInWithGoogle = () => signInWithPopup(auth, provider)
+const provider = new GoogleAuthProvider();
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
 
-export default firebase
+export default firebase;
