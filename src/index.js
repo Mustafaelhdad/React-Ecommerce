bba0f5a16cccd622a9f0f31f7d513ba6d960c0b4
mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2022-01-03 14:17:51 +0200

    make the checkout items exist after refreshing the page

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { persistGate } from 'redux-persist/integration/react'
 
 import './index.css'
 import App from './App'
 
-import store from './redux/store'
+import { store, persistor } from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <persistGate persistor={persistor}>
+        <App />
+      </persistGate>
     </BrowserRouter>
   </Provider >,
   document.getElementById('root')
