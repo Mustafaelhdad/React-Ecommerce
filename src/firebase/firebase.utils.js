a29eed93fb49b1f15f21c4d9c047d481ec9c2076
mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2022-02-22 18:13:21 +0200

    got the data out of firebase and formated it in a way we can use as routing and some other formats to use in the front end

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -55,6 +55,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   return await batch.commit()
 }
 
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data()
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  })
+
+  console.log(transformedCollection)
+}
+
 const provider = new GoogleAuthProvider();
 export const signInWithGoogle = () => signInWithPopup(auth, provider);
 
